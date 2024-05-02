b67814830a1591ca5603a88885870c70a4fd9110
Lugiad <adrien.grivel@hotmail.fr>
2024-05-02 02:35:14 +0200

    More accurate translation for weather.ts
    Added translations directly from French Scarlet / Voilet texts, and adapted the logic for the few lines that doesn't reallly have a translation in Scarlet / Violet

--- src/locales/fr/weather.ts
+++ src/locales/fr/weather.ts
@@ -4,41 +4,41 @@ import { SimpleTranslationEntries } from "#app/plugins/i18n";
  * The weather namespace holds text displayed when weather is active during a battle
  */
 export const weather: SimpleTranslationEntries = {
-    "sunnyStartMessage": "Les rayons du soleil brillent!",
-    "sunnyLapseMessage": "Les rayons du soleil brillent fort.",
-    "sunnyClearMessage": "Les rayons du soleil se dissipent.",
+    "sunnyStartMessage": "Les rayons du soleil brillent !",
+    "sunnyLapseMessage": "Les rayons du soleil sont forts !",
+    "sunnyClearMessage": "Les rayons du soleil s’affaiblissent !",
 
-    "rainStartMessage": "Il commence à pleuvoir!",
-    "rainLapseMessage": "La pluie continue.",
-    "rainClearMessage": "La pluie s'est arrêtée.",
+    "rainStartMessage": "Il commence à pleuvoir !",
+    "rainLapseMessage": "Il pleut !",
+    "rainClearMessage": "La pluie s’est arrêtée !",
 
-    "sandstormStartMessage": "Une tempête de sable se prépare!",
-    "sandstormLapseMessage": "La tempête de sable fait rage.",
-    "sandstormClearMessage": "La tempête de sable se calme.",
-    "sandstormDamageMessage": "{{pokemonPrefix}}{{pokemonName}} subit les dégâts\nde la tempête de sable!",
+    "sandstormStartMessage": "Une tempête de sable se prépare !",
+    "sandstormLapseMessage": "Une tempête de sable fait rage !",
+    "sandstormClearMessage": "La tempête de sable se calme !",
+    "sandstormDamageMessage": "La tempête de sable inflige des dégâts\nà {{pokemonPrefix}}{{pokemonName}} !",
 
-    "hailStartMessage": "Il commence à grêler!",
-    "hailLapseMessage": "La grêle continue.",
-    "hailClearMessage": "La grêle s'est arrêtée.",
-    "hailDamageMessage": "{{pokemonPrefix}}{{pokemonName}} subit les dégâts\nde la grêle!",
+    "hailStartMessage": "Il commence à grêler !",
+    "hailLapseMessage": "Il grêle !",
+    "hailClearMessage": "La grêle s'est arrêtée !",
+    "hailDamageMessage": "La grêle inflige des dégâts\nà {{pokemonPrefix}}{{pokemonName}} !",
 
-    "snowStartMessage": "Il commence à neiger!",
-    "snowLapseMessage": "La neige continue de tomber.",
-    "snowClearMessage": "La neige s'est arrêtée.",
+    "snowStartMessage": "Il y a une tempête de neige !",
+    "snowLapseMessage": "Il neige !",
+    "snowClearMessage": "La neige s’est arrêtée !",
 
-    "fogStartMessage": "Un brouillard épais émerge!",
-    "fogLapseMessage": "Le brouillard continue.",
-    "fogClearMessage": "Le brouillard a disparu.",
+    "fogStartMessage": "Le brouillard devient épais...",
+    "fogLapseMessage": "Il y a du brouillard !",
+    "fogClearMessage": "Le brouillard s’est dissipé !",
 
-    "heavyRainStartMessage": "Une pluie battante s'abat soudainement!",
-    "heavyRainLapseMessage": "La pluie battante continue.",
-    "heavyRainClearMessage": "La pluie battante s'est arrêtée.",
+    "heavyRainStartMessage": "Une pluie battante s’abat soudainement !",
+    "heavyRainLapseMessage": "La pluie est battante !",
+    "heavyRainClearMessage": "La pluie battante s’est arrêtée...",
     
-    "harshSunStartMessage": "Les rayons du soleil s'intensifient!",
-    "harshSunLapseMessage": "Le soleil est brûlant.",
-    "harshSunClearMessage": "Le soleil brûlant se dissipe.",
+    "harshSunStartMessage": "Les rayons du soleil s’intensifient !",
+    "harshSunLapseMessage": "Les rayons du soleil sont puissants !",
+    "harshSunClearMessage": "Les rayons du soleil s’affaiblissent !",
 
-    "strongWindsStartMessage": "Un vent violent se lève!",
-    "strongWindsLapseMessage": "Le vent souffle violemment.",
-    "strongWindsClearMessage": "Le vent violent s'est arrêté."
+    "strongWindsStartMessage": "Un vent mystérieux se lève !",
+    "strongWindsLapseMessage": "Le vent mystérieux souffle violemment !",
+    "strongWindsClearMessage": "Le vent mystérieux s’est dissipé..."
 }
